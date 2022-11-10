77238bbf0be4535025e6332da10868f953989734
João Tristão <jvtristaoac@gmail.com>
2022-11-09 22:16:34 -0300

    Adicionar arquivos de projeto.

---
+++ RPG/RPG/View/Dices.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RPG.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Dices : ContentPage
+    {
+        public Dices()
+        {
+            InitializeComponent();
+        }
+
+        private void d4_Clicked(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            DisplayAlert(random.Next(1, 5).ToString(), "", "OK");
+        }
+
+        private void d6_Clicked(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            DisplayAlert(random.Next(1, 7).ToString(), "", "OK");
+        }
+
+        private void d8_Clicked(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            DisplayAlert(random.Next(1, 9).ToString(), "", "OK");
+        }
+
+        private void d10_Clicked(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            DisplayAlert(random.Next(1, 11).ToString(), "", "OK");
+        }
+
+        private void d12_Clicked(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            DisplayAlert(random.Next(1, 13).ToString(), "", "OK");
+        }
+
+        private void d20_Clicked(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            DisplayAlert(random.Next(1, 21).ToString(), "", "OK");
+        }
+    }
+}
